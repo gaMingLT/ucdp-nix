81794ff07361f62cfb5753ae3305f4d153cee742
Milan Lagae <tech.github@intrusius.be>
2023-02-06 17:35:58 +0100

    Final configuration files

---
+++ default.nix
@@ -0,0 +1,25 @@
+let
+overlay = self: super: {
+   webapi = self.poetry2nix.mkPoetryApplication {
+     projectDir = ./.;
+     python = self.python3;
+   };
+  };
+  hostPkgs = import <nixpkgs> { overlays = [ overlay ]; };
+  linuxPkgs = import <nixpkgs> { overlays = [overlay ]; system = "x86_64-linux"; };
+in
+{
+ inherit (hostPkgs) webapi;
+
+ docker = hostPkgs.dockerTools.buildImage {
+   name = "webapi";
+   tag = "testing";
+   created = "now";
+
+   contents = linuxPkgs.webapi;
+   
+   config.Cmd = [ "webapi" ];
+   #config.cmd = [ "${linuxPkgs.videocopy}/videocopy" ];	 
+  };
+}
+
---
+++ web_api/main.py
@@ -0,0 +1,29 @@
+#import uvicorn
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+
+from fastapi import FastAPI
+
+config = Config()
+config.bind = ["0.0.0.0:8080"]
+config.accesslog = "-"
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/hello")
+async def hello():
+    return {"message": "hello world"}
+
+
+def start():
+    print("Starting application!")
+    asyncio.run(serve(app,config))
+    # uvicorn.run("video_copy.main:app",host="0.0.0.0", port=8080, reload=True)
+
